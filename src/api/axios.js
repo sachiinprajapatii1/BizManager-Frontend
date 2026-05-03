010ed6fac85e496b8b1f5040554d1da64a35f26c
Sachin <sachiinprajapatii@gmail.com>
2026-05-03 11:51:49 +0530

    Add Backend Deploy Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://bizmanager-backend.onrender.com',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 });
